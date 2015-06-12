68b1dd074b2afba710c687a8d52193e576c7dc71
xenon <moritz.goeckel@gmail.com>
2015-06-12 17:11:08 +0200

    Basic

--- Block.js
+++ Block.js
@@ -3,20 +3,24 @@ function Block(position, size)
     this.position = position;
     this.size = size;
 
-    var geometry = new THREE.CubeGeometry(size.x, size.y, size.z);
+    var geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
     var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
     this.mesh = new THREE.Mesh( geometry, material );
     this.mesh.castShadow = true;
-    this.mesh.position = this.position;
 
     this.update = function()
     {
         //Update Mesh
-        this.mesh.position = this.position;
+        this.mesh.position.x = this.position.x;
+        this.mesh.position.y = this.position.y;
+        this.mesh.position.z = this.position.z;
+
     };
 
     this.addToScene = function(scene)
     {
         scene.add(this.mesh);
     };
+
+    this.update();
 }
