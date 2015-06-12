1f833c936aa0bb03ce70e53f657b4757e44ce5c3
xenon <moritz.goeckel@gmail.com>
2015-06-12 17:31:55 +0200

    added the function to change size of block

--- Block.js
+++ Block.js
@@ -1,12 +1,8 @@
-function Block(position, size)
+function Block(position, size, scene)
 {
     this.position = position;
     this.size = size;
-
-    var geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
-    var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-    this.mesh = new THREE.Mesh( geometry, material );
-    this.mesh.castShadow = true;
+    this.scene = scene;
 
     this.update = function()
     {
@@ -14,13 +10,27 @@ function Block(position, size)
         this.mesh.position.x = this.position.x;
         this.mesh.position.y = this.position.y;
         this.mesh.position.z = this.position.z;
-
     };
 
-    this.addToScene = function(scene)
-    {
-        scene.add(this.mesh);
+    this.setSize = function(h){
+      scene.remove(this.mesh);
+      this.size.z = h;
+
+      this.init();
+      this.update();
+      
+      this.scene.add(this.mesh);
     };
 
+    this.init = function(){
+      var geometry = new THREE.BoxGeometry(this.size.x, this.size.y, this.size.z);
+      var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+      this.mesh = new THREE.Mesh( geometry, material );
+      this.mesh.castShadow = true;
+    };
+
+    this.init();
     this.update();
+
+    this.scene.add(this.mesh);
 }
